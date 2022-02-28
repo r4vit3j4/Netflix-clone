5297a50d1d00e5e83e39e973fbd29fe81c25e3a0
Ravi Teja <75667275+r4vit3j4@users.noreply.github.com>
2022-02-28 17:03:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHMwuieA_gAFrOX_Engi74LTd6b-tN_No",
-  authDomain: "netflix-clone-95057.firebaseapp.com",
-  projectId: "netflix-clone-95057",
-  storageBucket: "netflix-clone-95057.appspot.com",
-  messagingSenderId: "894226587489",
-  appId: "1:894226587489:web:8418487b1f089b69c92f20",
+//     firebase config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
